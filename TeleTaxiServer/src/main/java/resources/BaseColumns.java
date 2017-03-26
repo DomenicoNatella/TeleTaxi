88f6f38f885c9e134ab74e83dff88f0f7206c680
Domenico Natella <domenico.natella@gmail.com>
2017-03-26 19:21:26 +0200

    Resources of TeleTaxi Server

--- TeleTaxiServer/src/main/java/resources/BaseColumns.java
+++ TeleTaxiServer/src/main/java/resources/BaseColumns.java
@@ -11,7 +11,28 @@ public class BaseColumns {
     public static final String NOME_PERSONA = "nome";
     public static final String COGNOME_PERSONA = "cognome";
     public static final String DATA_DI_NASCITA_PERSONA = "data_di_nascita";
-    public static final String PASSWORD = "password";
+    public static final String PASSWORD = "chiave_di_accesso";
+
+
+    public static final String TAB_PRENOTAZIONI = "prenotazioni";
+    public static final String PROGRESSIVO_PRENOTAZIONE = "progressivo";
+    public static final String TELEFONO = "telefono";
+    public static final String POSIZIONE_CORRENTE = "posizione_corrente";
+    public static final String SERVIZI_SPECIALI = "servizi_speciali" ;
+
+
+    public static final String TAB_CLIENTE = "clienti";
+    public static final String IDENTIFICATIVO_CLIENTE = "codice_cliente" ;
+
+    public static final String TAB_TAXI = "taxi";
+    public static final String IDENTIFICATIVO_TAXI = "identificativo";
+    public static final String STATO_TAXI = "stato" ;
+    public static final String DESTINAZIONE = "destinazione";
+
+    public static final String TAB_MANAGER = "manager";
+    public static final String USERNAME_MANAGER = "username";
+
+    public static final String DB_NAME = "TELETAXI" ;
 
 
 }
