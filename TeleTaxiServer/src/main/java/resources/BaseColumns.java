4f5e67b37eed9717a0a578ab19caf345fc3d8969
Domenico Natella <domenico.natella@gmail.com>
2017-03-26 09:52:25 +0200

    Resources of TeleTaxi Server

---
+++ TeleTaxiServer/src/main/java/resources/BaseColumns.java
@@ -0,0 +1,17 @@
+package resources;
+
+/**
+ * Created by dn on 21/03/17.
+ */
+public class BaseColumns {
+
+    public static final String TAB_OPERATORI_TELEFONICI = "operatori_telefonici";
+    public static final String IDENTIFICATIVO_OPERATORE_TELEFONICO = "identificativo";
+
+    public static final String NOME_PERSONA = "nome";
+    public static final String COGNOME_PERSONA = "cognome";
+    public static final String DATA_DI_NASCITA_PERSONA = "data_di_nascita";
+    public static final String PASSWORD = "password";
+
+
+}
